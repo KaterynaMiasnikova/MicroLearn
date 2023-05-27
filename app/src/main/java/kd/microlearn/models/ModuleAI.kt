a301df1226570b761ddc40657085454a8e1f2875
Daria Melnyk <daria.melnyk.a@gmail.com>
2023-05-27 19:55:41 +0200

    feedback model and store added

--- app/src/main/java/kd/microlearn/models/ModuleAI.kt
+++ app/src/main/java/kd/microlearn/models/ModuleAI.kt
@@ -1,4 +1,86 @@
-package kd.microlearn.models
-
-class ModuleAI {
-}
+//package kd.microlearn.models
+//
+//import edu.stanford.nlp.pipeline.CoreDocument
+//import edu.stanford.nlp.pipeline.StanfordCoreNLP
+//import org.apache.commons.text.similarity.CosineSimilarity
+//
+//class ModuleAI(private val database: AppDatabase) {
+//    private val cosineSimilarity = CosineSimilarity()
+//    private val pipeline: StanfordCoreNLP
+//
+//    init {
+//        val properties = Properties()
+//        properties.setProperty("annotators", "tokenize, ssplit")
+//        pipeline = StanfordCoreNLP(properties)
+//    }
+//
+//    fun processData(data: String): List<String> {
+//        val annotation = annotateText(data)
+//        val sentences = extractSentences(annotation)
+//        val orderedPortions = database.portionDao().getAll().map { it.portion }
+//
+//        val regeneratedMaterial = generateLogicalMaterial(orderedPortions, sentences)
+//
+//        // Save the regenerated material to the database
+//        val startIndex = orderedPortions.size
+//        regeneratedMaterial.forEachIndexed { index, portion ->
+//            val portionModel = PortionModel(startIndex + index, portion)
+//            database.portionDao().insert(portionModel)
+//        }
+//
+//        return regeneratedMaterial
+//    }
+//
+//    fun regenerateMaterial(material: String, userFeedback: Feedback): List<String> {
+//        val orderedPortions = database.portionDao().getAll().map { it.portion }
+//        val startIndex = orderedPortions.indexOf(material) + 1
+//        val regeneratedMaterial = generateLogicalMaterial(orderedPortions, startIndex, userFeedback)
+//
+//        // Save the regenerated material to the database
+//        regeneratedMaterial.forEachIndexed { index, portion ->
+//            val portionModel = PortionModel(startIndex + index, portion)
+//            database.portionDao().insert(portionModel)
+//        }
+//
+//        return regeneratedMaterial
+//    }
+//
+//    private fun annotateText(text: String): Annotation {
+//        val annotation = Annotation(text)
+//        pipeline.annotate(annotation)
+//        return annotation
+//    }
+//
+//    private fun extractSentences(annotation: Annotation): List<String> {
+//        val sentences = mutableListOf<String>()
+//        val sentenceAnnotations = annotation.get(CoreAnnotations.SentencesAnnotation::class.java)
+//        for (sentenceAnnotation in sentenceAnnotations) {
+//            val sentence = sentenceAnnotation.get(CoreAnnotations.TextAnnotation::class.java)
+//            sentences.add(sentence)
+//        }
+//        return sentences
+//    }
+//
+//    private fun generateLogicalMaterial(sentences: List<String>, startIndex: Int, userFeedback: Feedback? = null): List<String> {
+//        val logicalMaterial = mutableListOf<String>()
+//
+//        for (i in startIndex until sentences.size) {
+//            val lastSentence = logicalMaterial.lastOrNull()
+//            val currentSentence = sentences[i]
+//            if (lastSentence != null && isContinuationOfLogicalContent(lastSentence, currentSentence, userFeedback)) {
+//                logicalMaterial[logicalMaterial.size - 1] = "$lastSentence $currentSentence"
+//            } else {
+//                logicalMaterial.add(currentSentence)
+//            }
+//        }
+//
+//        return logicalMaterial
+//    }
+//
+//    private fun isContinuationOfLogicalContent(previousSentence: String, nextSentence: String, userFeedback: Feedback?): Boolean {
+//        val similarityScore = cosineSimilarity.cosineSimilarity(previousSentence, nextSentence)
+//        val similarityThreshold = if (userFeedback?.note ?: 0 < 4) 0.6 else 0.8
+//
+//        return similarityScore >= similarityThreshold && (userFeedback?.summary.isNullOrBlank() || userFeedback?.summary == nextSentence)
+//    }
+//}
---
+++ app/src/main/java/kd/microlearn/models/WikipediaDataRetriever.kt
@@ -0,0 +1,26 @@
+package kd.microlearn.models
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+
+class WikipediaDataRetriever {
+    fun retrieveData(theme: String): String? {
+        val url = getWikipediaURL(theme)
+        val document = fetchWebPage(url)
+        return extractContent(document)
+    }
+
+    private fun getWikipediaURL(theme: String): String {
+        val encodedTheme = theme.replace(" ", "_")
+        return "https://en.wikipedia.org/wiki/$encodedTheme"
+    }
+
+    private fun fetchWebPage(url: String): Document {
+        return Jsoup.connect(url).get()
+    }
+
+    private fun extractContent(document: Document): String? {
+        val contentElement = document.getElementById("mw-content-text")
+        return contentElement?.text()
+    }
+}
